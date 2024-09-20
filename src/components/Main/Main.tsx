499e9fcdfd0056ac85ce1fe927470e1dfdca4044
Igor Marines <igor.marines25@gmail.com>
2024-09-20 02:31:25 -0300

    feat: :sparkles: Modal de confirmação para executar o NPM RUN SERVER (servidor fake db)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,55 @@
+"use client";
+
 import './styles.scss';
+import { Modal } from '../Modal/Modal';
+import { Button } from '../Button/Button';
+import { useState } from 'react';
 
 export default function Main({ children }: Readonly<{ children: React.ReactNode }>) {
+
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const getAcceptedWarnFromLocalStorage = () => {
+        return localStorage.getItem('acceptedWarn');
+    }
+
+    const setAcceptedWarnToLocalStorage = () => {
+        localStorage.setItem('acceptedWarn', 'true');
+    }
+
+    const userReadWarn = getAcceptedWarnFromLocalStorage();
+
+    const copyToClipboard = (text: string) => {
+        navigator.clipboard.writeText(text).then(() => {
+            alert('Texto copiado para a área de transferência');
+        }).catch(() => {
+            alert('Erro ao copiar texto');
+        });
+    }
+
+
+
     return (
         <div className='container'>
+
+            {!userReadWarn && isModalOpen &&
+                <Modal
+                    title='Aviso'
+                    isOpen={isModalOpen}
+                    onClose={() => { }}
+                >
+                    <p style={{ padding: '16px 0' }}>
+                        Para iniciar o servidor e criar tarefas, execute o comando <code style={{ color: 'white', background: "gray", padding: '3px', cursor: 'pointer' }} onClick={() => copyToClipboard('NPM RUN SERVER')}>
+                            NPM RUN SERVER</code> no terminal.
+                    </p>
+                    <Button action={() => {
+                        setAcceptedWarnToLocalStorage()
+                        setIsModalOpen(false)
+                    }
+
+                    } text='Estou ciente' variant='create' />
+                </Modal>
+            }
             <main>
                 {children}
             </main>
